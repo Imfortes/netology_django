665b1a2029b7dfa8f801e68f83365a25ec875058
Imfortes <imfortes@list.ru>
2025-07-03 23:57:59 +0300

    Набросал страницу рецепта и начал считать пропорции на порцию

--- first_project/recipe_book/views.py
+++ first_project/recipe_book/views.py
@@ -7,6 +7,7 @@ def recipes(request):
     recipes_book = {
         'Завтраки': {
             'Омлет': {
+                'id': 1,
                 'slug': 'eggs',
                 'ingredients': {
                     'Яйцо': '2 шт',
@@ -15,6 +16,7 @@ def recipes(request):
                 }
             },
             'Блинчики': {
+                'id': 2,
                 'slug': 'pancakes',
                 'ingredients': {
                     'Мука': '200 гр',
@@ -26,6 +28,7 @@ def recipes(request):
 
         'Обеды': {
             'Суп': {
+                'id': 3,
                 'slug': 'soup',
                 'ingredients': {
                     'Картофель': '3 шт',
@@ -37,5 +40,45 @@ def recipes(request):
 
     return render(request, 'recipe_book/recipes.html', {'recipes': recipes_book})
 
-def recipe_detail(request):
-    return render(request, 'recipe_book/recipe_detail.html')
+def recipe_detail(request, slug):
+    recipes_book = {
+        'Завтраки': {
+            'Омлет': {
+                'id': 1,
+                'slug': 'eggs',
+                'ingredients': {
+                    'Яйцо': '2 шт',
+                    'Сыр': '50 гр',
+                    'Молоко': '100 мл'
+                }
+            },
+            'Блинчики': {
+                'id': 2,
+                'slug': 'pancakes',
+                'ingredients': {
+                    'Мука': '200 гр',
+                    'Яйцо': '1 шт',
+                    'Молоко': '300 мл'
+                }
+            },
+        },
+
+        'Обеды': {
+            'Суп': {
+                'id': 3,
+                'slug': 'soup',
+                'ingredients': {
+                    'Картофель': '3 шт',
+                    'Морковь': '1 шт'
+                }
+            }
+        }
+    }
+
+    recipe = None
+    for category in recipes_book.values():
+        for name, data in category.items():
+            if data['slug'] == slug:
+                recipe = {'name': name, **data}
+
+    return render(request, 'recipe_book/recipe_detail.html', {'recipe': recipe})
